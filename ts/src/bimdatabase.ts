4cc350027ab420d32d49a62307632e82c0df1ff6
Ondřej Hošek <ondra.hosek@gmail.com>
2023-10-12 00:22:30 +0200

    TypeScript creature comforts for editing couplings

--- ts/src/bimdatabase.ts
+++ ts/src/bimdatabase.ts
@@ -1,106 +1,13 @@
+import { AddEdit } from './add_edit';
+import { CouplingAddEdit } from './coupling_add_edit';
+
 export module BimDatabase {
-    function submitAddEdit(
-        form: HTMLFormElement,
-        otherDataParent: HTMLElement,
-        otherDataTextArea: HTMLTextAreaElement,
-        ev: SubmitEvent,
-    ) {
-        ev.preventDefault();
-
-        // reassemble text area value
-        const otherEntryPieces: HTMLElement[] = Array.prototype.slice.call(otherDataParent.querySelectorAll("div.other-data-entry"), 0);
-        const obj = {};
-        for (let paragraph of otherEntryPieces) {
-            const keyInput = <HTMLInputElement|null>paragraph.querySelector("input.key");
-            if (keyInput === null) {
-                continue;
-            }
-
-            const valueInput = <HTMLInputElement|null>paragraph.querySelector("input.value");
-            if (valueInput === null) {
-                continue;
-            }
-
-            obj[keyInput.value] = valueInput.value;
-        }
-        otherDataTextArea.value = JSON.stringify(obj);
-
-        // remove custom form fields
-        for (let paragraph of otherEntryPieces) {
-            paragraph.parentNode?.removeChild(paragraph);
-        }
-
-        // submit modified form
-        form.submit();
-    }
-
-    function addOtherDataEntry(otherDataParent: HTMLElement, newEntryContainer: HTMLElement): [HTMLInputElement, HTMLInputElement] {
-        const entryContainer: HTMLElement = document.createElement("div");
-        entryContainer.classList.add("other-data-entry");
-        otherDataParent.insertBefore(entryContainer, newEntryContainer);
-
-        const keyInput = document.createElement("input");
-        keyInput.type = "text";
-        keyInput.classList.add("key");
-        entryContainer.appendChild(keyInput);
-
-        const valueInput = document.createElement("input");
-        valueInput.type = "text";
-        valueInput.classList.add("value");
-        entryContainer.appendChild(valueInput);
-
-        const minusButton = document.createElement("input");
-        minusButton.type = "button";
-        minusButton.value = "\u2212";
-        minusButton.addEventListener("click", () => entryContainer.parentNode?.removeChild(entryContainer));
-        entryContainer.appendChild(minusButton);
-
-        return [keyInput, valueInput];
-    }
-
-    function doSetUpAddEdit() {
-        const otherDataTextArea = <HTMLTextAreaElement|null>document.getElementById("bimdb-ae-other-data");
-        if (otherDataTextArea === null) {
-            return;
-        }
-        const otherDataParent = otherDataTextArea.parentElement;
-        if (otherDataParent === null) {
-            return;
-        }
-        const form = otherDataTextArea.form;
-        if (form === null) {
-            return;
-        }
-
-        form.addEventListener("submit", ev => submitAddEdit(form, otherDataParent, otherDataTextArea, ev));
-
-        // add new-entry piece
-        const newEntryContainer = document.createElement("div");
-        newEntryContainer.classList.add("add-other-data-entry");
-        otherDataParent.appendChild(newEntryContainer);
-
-        // disassemble text area
-        const otherDataJson = JSON.parse(otherDataTextArea.value);
-        const otherDataKeys = Object.keys(otherDataJson);
-        for (let otherDataKey of otherDataKeys) {
-            const otherDataValue = otherDataJson[otherDataKey];
-
-            const [keyInput, valueInput] = addOtherDataEntry(otherDataParent, newEntryContainer);
-            keyInput.value = otherDataKey;
-            valueInput.value = otherDataValue;
-        }
-
-        const plusButton = document.createElement("input");
-        plusButton.type = "button";
-        plusButton.value = "+";
-        plusButton.addEventListener("click", () => addOtherDataEntry(otherDataParent, newEntryContainer));
-        newEntryContainer.appendChild(plusButton);
-
-        otherDataTextArea.style.display = "none";
-    }
-
     export function setUpAddEdit() {
-        document.addEventListener("DOMContentLoaded", doSetUpAddEdit);
+        document.addEventListener("DOMContentLoaded", AddEdit.doSetUp);
+    }
+
+    export function setUpCouplingAddEdit() {
+        document.addEventListener("DOMContentLoaded", CouplingAddEdit.doSetUp);
     }
 }
 
---
+++ ts/src/coupling_add_edit.ts
@@ -0,0 +1,137 @@
+export module CouplingAddEdit {
+    function handleSubmit(
+        form: HTMLFormElement,
+        vehiclesParent: HTMLElement,
+        vehiclesTextArea: HTMLTextAreaElement,
+        ev: SubmitEvent,
+    ) {
+        ev.preventDefault();
+
+        // reassemble text area value
+        const vehicleEntries: HTMLElement[] = Array.prototype.slice.call(vehiclesParent.querySelectorAll(".vehicle-entry"), 0);
+        const numbers: string[] = [];
+        for (let vehicleEntry of vehicleEntries) {
+            const numberInput = <HTMLInputElement|null>vehicleEntry.querySelector("input.vehicle-number");
+            if (numberInput === null) {
+                continue;
+            }
+            numbers.push(numberInput.value);
+        }
+        vehiclesTextArea.value = numbers.join("\n");
+
+        // remove custom form fields
+        for (let vehicleEntry of vehicleEntries) {
+            vehicleEntry.parentNode?.removeChild(vehicleEntry);
+        }
+
+        // submit modified form
+        form.submit();
+    }
+
+    function enableDisableUpDown(vehiclesParent: HTMLElement) {
+        const vehicleEntries = vehiclesParent.querySelectorAll(".vehicle-entry");
+        for (let i = 0; i < vehicleEntries.length; i++) {
+            const vehicleEntry = vehicleEntries.item(i);
+            const upButton = <HTMLInputElement|null>vehicleEntry.querySelector(".up-button");
+            if (upButton !== null) {
+                upButton.disabled = (i === 0);
+            }
+            const downButton = <HTMLInputElement|null>vehicleEntry.querySelector(".down-button");
+            if (downButton !== null) {
+                downButton.disabled = (i === vehicleEntries.length - 1);
+            }
+        }
+    }
+
+    function moveUp(vehiclesParent: HTMLElement, entryContainer: HTMLElement) {
+        entryContainer.parentNode?.insertBefore(entryContainer, entryContainer.previousElementSibling);
+        enableDisableUpDown(vehiclesParent);
+    }
+
+    function moveDown(vehiclesParent: HTMLElement, entryContainer: HTMLElement) {
+        const next = entryContainer.nextElementSibling;
+        const nextNext = (next !== null) ? next.nextElementSibling : null;
+        entryContainer.parentNode?.insertBefore(entryContainer, nextNext);
+        enableDisableUpDown(vehiclesParent);
+    }
+
+    function remove(vehiclesParent: HTMLElement, entryContainer: HTMLElement) {
+        entryContainer.parentNode?.removeChild(entryContainer);
+        enableDisableUpDown(vehiclesParent);
+    }
+
+    function addVehicle(vehiclesParent: HTMLElement, newEntryContainer: HTMLElement): HTMLInputElement {
+        const entryContainer: HTMLElement = document.createElement("div");
+        entryContainer.classList.add("vehicle-entry");
+        vehiclesParent.insertBefore(entryContainer, newEntryContainer);
+
+        const numberInput = document.createElement("input");
+        numberInput.type = "text";
+        numberInput.classList.add("vehicle-number");
+        entryContainer.appendChild(numberInput);
+
+        const minusButton = document.createElement("input");
+        minusButton.type = "button";
+        minusButton.value = "\u2212";
+        minusButton.addEventListener("click", () => remove(vehiclesParent, entryContainer));
+        entryContainer.appendChild(minusButton);
+
+        const upButton = document.createElement("input");
+        upButton.type = "button";
+        upButton.classList.add("up-button");
+        upButton.value = "\u2191";
+        upButton.addEventListener("click", () => moveUp(vehiclesParent, entryContainer));
+        entryContainer.appendChild(upButton);
+
+        const downButton = document.createElement("input");
+        downButton.type = "button";
+        downButton.classList.add("down-button");
+        downButton.value = "\u2193";
+        downButton.addEventListener("click", () => moveDown(vehiclesParent, entryContainer));
+        entryContainer.appendChild(downButton);
+
+        enableDisableUpDown(vehiclesParent);
+
+        return numberInput;
+    }
+
+    export function doSetUp() {
+        const vehiclesTextArea = <HTMLTextAreaElement|null>document.getElementById("bimdb-cae-vehicles");
+        if (vehiclesTextArea === null) {
+            return;
+        }
+        const vehiclesParent = vehiclesTextArea.parentElement;
+        if (vehiclesParent === null) {
+            return;
+        }
+        const form = vehiclesTextArea.form;
+        if (form === null) {
+            return;
+        }
+
+        form.addEventListener("submit", ev => handleSubmit(form, vehiclesParent, vehiclesTextArea, ev));
+
+        // add new-entry piece
+        const newEntryContainer = document.createElement("div");
+        newEntryContainer.classList.add("add-vehicle");
+        vehiclesParent.appendChild(newEntryContainer);
+
+        // disassemble text area
+        const vehicleNumbers = vehiclesTextArea.value
+            .split("\n")
+            .map(vn => vn.trim())
+            .filter(vn => vn.length > 0);
+        for (let vehicleNumber of vehicleNumbers) {
+            const vehicleNumberInput = addVehicle(vehiclesParent, newEntryContainer);
+            vehicleNumberInput.value = vehicleNumber;
+        }
+
+        const plusButton = document.createElement("input");
+        plusButton.type = "button";
+        plusButton.value = "+";
+        plusButton.addEventListener("click", () => addVehicle(vehiclesParent, newEntryContainer));
+        newEntryContainer.appendChild(plusButton);
+
+        vehiclesTextArea.style.display = "none";
+    }
+}
